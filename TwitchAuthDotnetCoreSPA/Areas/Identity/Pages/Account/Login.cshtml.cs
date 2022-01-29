e9875d6a4c38a873bc4368ddd09cea8e6d7566fc
Chris Schaefer <schaefer007@gmail.com>
2022-01-28 21:33:13 -0500

    Extract code into a service

--- TwitchAuthDotnetCoreSPA/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ TwitchAuthDotnetCoreSPA/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -12,6 +12,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Http;
+using TwitchAuthDotnetCoreSPA.Services;
 
 namespace TwitchAuthDotnetCoreSPA.Areas.Identity.Pages.Account
 {
@@ -20,12 +21,12 @@ namespace TwitchAuthDotnetCoreSPA.Areas.Identity.Pages.Account
     {
 		public IHttpContextAccessor _context { get; }
 
-		private readonly TwitchSettings _twitchSettings;
+		private readonly TwitchService _twitchService;
 
-		public LoginModel(IHttpContextAccessor context, TwitchSettings twitchSettings)
+		public LoginModel(IHttpContextAccessor context, TwitchService twitchService)
         {
             _context = context;
-            _twitchSettings = twitchSettings;
+            _twitchService = twitchService;
         }
 
         [BindProperty]
@@ -54,61 +55,8 @@ namespace TwitchAuthDotnetCoreSPA.Areas.Identity.Pages.Account
 
         public RedirectResult OnGetAsync(string returnUrl = null)
         {
-            var request = _context.HttpContext.Request;
-            var _baseURL = $"{request.Scheme}://{request.Host}"; // http://localhost:5000
-            var redirectURL = $"{_baseURL}/twitch/auth";
-            return Redirect($"https://id.twitch.tv/oauth2/authorize?client_id={_twitchSettings.ClientId}&redirect_uri={redirectURL}&response_type=code&scope=user:read:email&force_verify=true");
-            /*
-            if(!string.IsNullOrEmpty(ErrorMessage))
-            {
-                ModelState.AddModelError(string.Empty, ErrorMessage);
-            }
-
-            returnUrl ??= Url.Content("~/");
-
-            // Clear the existing external cookie to ensure a clean login process
-            await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
-
-            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
-
-            ReturnUrl = returnUrl;
-            */
-        }
-
-        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
-        {/*
-            returnUrl ??= Url.Content("~/");
-
-            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
-        
-            if (ModelState.IsValid)
-            {
-                // This doesn't count login failures towards account lockout
-                // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
-                if (result.Succeeded)
-                {
-                    _logger.LogInformation("User logged in.");
-                    return LocalRedirect(returnUrl);
-                }
-                if (result.RequiresTwoFactor)
-                {
-                    return RedirectToPage("./LoginWith2fa", new { ReturnUrl = returnUrl, RememberMe = Input.RememberMe });
-                }
-                if (result.IsLockedOut)
-                {
-                    _logger.LogWarning("User account locked out.");
-                    return RedirectToPage("./Lockout");
-                }
-                else
-                {
-                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
-                    return Page();
-                }
-            }*/
-
-            // If we got this far, something failed, redisplay form
-            return Page();
+            string redirectUrl = _twitchService.GetTwitchLoginUrl();
+            return Redirect(redirectUrl);
         }
     }
 }
